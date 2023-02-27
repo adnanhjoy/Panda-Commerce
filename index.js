a100cd60388db2161e7426f225f06db7aed8d2aa
engadnanhossain <engr.adnanhossain@gmail.com>
2023-02-27 17:44:47 +0600

    added js file

--- index.js
+++ index.js
@@ -1,37 +1,38 @@
-const h2Color = document.querySelectorAll('h2');
-for (h2 of h2Color) {
-    h2.style.color = 'lightblue';
-}
-const backpack = document.getElementById('backpacks');
-backpack.style.backgroundColor = 'yellow';
+// const h2Color = document.querySelectorAll('h2');
+// for (h2 of h2Color) {
+//     h2.style.color = 'lightblue';
+// }
+// const backpack = document.getElementById('backpacks');
+// backpack.style.backgroundColor = 'yellow';
 
-const cards = document.getElementsByClassName('card');
-for (card of cards) {
-    card.style.borderRadius = '30px'
-}
+// const cards = document.getElementsByClassName('card');
+// for (card of cards) {
+//     card.style.borderRadius = '30px'
+// }
 
-function buyNow() {
-    console.log('Hello')
+// function buyNow() {
+//     console.log('Hello')
 
 
-}
+// }
 
-const buyNowButton = document.querySelectorAll('a');
-for (button of buyNowButton) {
-    button.addEventListener('click', function () {
-        const cards = document.getElementsByClassName('card');
-        for (card of cards) {
-            card.style.display = 'none'
-        }
-    })
-}
+// const buyNowButton = document.querySelectorAll('a');
+// for (button of buyNowButton) {
+//     button.addEventListener('click', function () {
+//         const cards = document.getElementsByClassName('card');
+//         for (card of cards) {
+//             card.style.display = 'none'
+//         }
+//     })
+// }
 
 document.getElementById('input-field').addEventListener('keyup', function (event) {
     text = event.target.value;
-    const submited = document.getElementById('delete-btn');
-    if (text === 'delete') {
-        submited.removeAttribute('disabled');
+    console.log(text)
+    const submitEmail = document.getElementById('submit-email');
+    if (text === 'email') {
+        submitEmail.removeAttribute('disabled');
     } else {
-        submited.setAttribute('disabled', true);
+        submitEmail.setAttribute('disabled', true)
     }
 })
